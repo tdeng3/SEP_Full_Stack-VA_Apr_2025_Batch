f2a59d9505e1ce6a47fcf08f8c81a24d0c711909
Tianyuan Deng <49263084+tdeng3@users.noreply.github.com>
2025-04-30 18:04:59 -0500

    Update 2nd_SQL_Assignment.sql

--- 2nd_SQL_Assignment.sql
+++ 2nd_SQL_Assignment.sql
@@ -1,8 +1,6 @@
 USE AdventureWorks2019
 GO
 
--- AGGREGATION FIELD 
-    -- COUNT *
 --SELECT fields, aggregation(field)
 --FROM table JOIN table2 on..
 --WHERE Criteria -- optional
@@ -10,7 +8,7 @@ GO
 --HAVING criteia --Optional
 --ORDER BY fields DESC --optional
 
---EXCUTE ORDER
+--EXCUTION ORDER
 --FROM-->WHERE-->GROUP BY-->HAVING-->SELECT-->DISTINCE-->ORDER BY
 
 
@@ -153,34 +151,105 @@ GO
     -- 13. Write a query that lists the country and province names from person.CountryRegion and person.StateProvince tables 
         --  and list the countries filter them by Germany and Canada. 
         --  Join them and produce a result set similar to the following.
-
+        
         -- Country                        Province
 
         ---------                          ----------------------
 
         SELECT pcr.Name AS Country, psp.Name AS Province 
-        FROM Person.CountryRegion pcr JOIN Person.StateProvince psp ON pcr.CountryRegionCode = psp.CountryRegionCode 
+        FROM Person.CountryRegion AS pcr JOIN Person.StateProvince AS psp ON pcr.CountryRegionCode = psp.CountryRegionCode 
         WHERE pcr.Name IN ('Germany','Canada')
 
-        -- Using Northwnd Database: (Use aliases for all the Joins)
+--  Using Northwnd Database: (Use aliases for all the Joins)
+
+USE Northwind
+GO
 
     -- 14. List all Products that has been sold at least once in last 27 years.
 
+        SELECT *
+        FROM Orders o
+
+        SELECT *
+        FROM [Order Details] od
+
+        SELECT DISTINCT p.ProductName, od.UnitPrice, o.OrderDate
+        FROM Products p JOIN [Order Details] od ON P.ProductID = od.ProductID 
+                        JOIN Orders o ON od.OrderID = o.OrderID
+        WHERE o.OrderDate >= DATEADD(YEAR, -27, GETDATE())
+
     -- 15. List top 5 locations (Zip Code) where the products sold most.
+
+        SELECT * 
+        FROM Products p JOIN [Order Details] od ON P.ProductID = od.ProductID 
+                        JOIN Orders o ON od.OrderID = o.OrderID
+
+        SELECT Top 5 o.ShipPostalCode, SUM(od.Quantity) AS [Total Sold]
+        FROM Products p JOIN [Order Details] od ON P.ProductID = od.ProductID 
+                        JOIN Orders o ON od.OrderID = o.OrderID
+        WHERE o.ShipPostalCode IS NOT NULL
+        GROUP BY o.ShipPostalCode
+        ORDER BY [Total Sold] DESC
 
     -- 16. List top 5 locations (Zip Code) where the products sold most in last 27 years.
 
+        SELECT Top 5 o.ShipPostalCode, SUM(od.Quantity) AS [Total Sold]
+        FROM Products p JOIN [Order Details] od ON P.ProductID = od.ProductID 
+                        JOIN Orders o ON od.OrderID = o.OrderID
+        WHERE o.ShipPostalCode IS NOT NULL AND o.OrderDate >= DATEADD(YEAR, -27, GETDATE())
+        GROUP BY o.ShipPostalCode
+        ORDER BY 2 DESC
+
     -- 17. List all city names and number of customers in that city.     
+
+        SELECT c.City, COUNT(c.CustomerID) AS [Total Customers]
+        FROM Customers c
+        GROUP BY c.City
+        ORDER BY 2 DESC
 
     -- 18. List city names which have more than 2 customers, and number of customers in that city
 
+        SELECT c.City, COUNT(c.CustomerID) AS [Total Customers]
+        FROM Customers c
+        GROUP BY c.City
+        HAVING COUNT(c.CustomerID) > 2
+        ORDER BY 2 DESC
+
     -- 19. List the names of customers who placed orders after 1/1/98 with order date.
+
+        SELECT o.OrderDate
+        FROM Orders o
+
+        SELECT c.ContactName, o.OrderDate
+        FROM Customers c JOIN Orders o ON c.CustomerID = o.CustomerID
+        WHERE o.OrderDate > '1998-01-01 00:00:00.000'
 
     -- 20. List the names of all customers with most recent order dates
 
+        SELECT c.ContactName, o.OrderDate
+        FROM Customers c JOIN Orders o ON c.CustomerID = o.CustomerID
+        WHERE o.OrderDate = (
+            SELECT MAX(o.OrderDate)
+            FROM Orders o
+            WHERE o.CustomerID = c.CustomerID
+        )
+        ORDER BY c.ContactName
+
     -- 21. Display the names of all customers  along with the  count of products they bought
 
+        SELECT c.ContactName
+        FROM Customers c
+
+        SELECT c.ContactName, COUNT(p.ProductName) AS [Products they bought]
+        FROM Products p JOIN [Order Details] od ON P.ProductID = od.ProductID 
+                        JOIN Orders o ON od.OrderID = o.OrderID 
+                        JOIN Customers c ON c.CustomerID = o.CustomerID
+        GROUP BY c.ContactName
+        ORDER BY 2 DESC
+        
     -- 22. Display the customer ids who bought more than 100 Products with count of products.
+
+        
 
     -- 23. List all of the possible ways that suppliers can ship their products. Display the results as below
 
