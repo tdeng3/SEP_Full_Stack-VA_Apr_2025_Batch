c23eaccf3a089ce24f8241e9a0279bede68a443a
Tianyuan Deng <49263084+tdeng3@users.noreply.github.com>
2025-05-07 18:16:49 -0500

    Create C#_1st_Assignment_Part2_Program.cs

---
+++ C#_1st_Assignment_Part2_Program.cs
@@ -0,0 +1,26 @@
+// See https://aka.ms/new-console-template for more information
+// Assignment 02 Arrays and Strings.pdf
+
+// Here are the answers to the coding questions.
+
+// Create a project named 02ArraysAndStrings.
+
+// Copy code from C#_1st_Assignment_Part2_Program.cs into Program.cs.
+
+// Create ArraysAndStrings.cs and copy relevant code from C#_1st_Assignment_Part2_ArraysAndStrings.cs.
+
+// Ensure Program.cs can use ArraysAndStrings.cs.
+
+
+using _02ArraysAndStrings;
+
+ArraysAndStrings arraysAndStrings = new ArraysAndStrings();
+arraysAndStrings.runCopyingAndArrays();
+arraysAndStrings.runShoppingCart();
+arraysAndStrings.runFindPrimeNumbers(1,14);
+arraysAndStrings.runArrayRotationSum();
+arraysAndStrings.runFindLongestSequence();
+ arraysAndStrings.runFindMostFrequentNumbers();
+ arraysAndStrings.runReverseString();
+ arraysAndStrings.runExtractUniquePalindromes();
+arraysAndStrings.runFindProtocolServerAndResource();
